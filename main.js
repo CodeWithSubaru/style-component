7a92ab2d4ee13b238279073e8d8db8f1fa2a8f7b
CodeWithSubaru <francis.cloydm@gmail.com>
2022-09-26 11:55:59 +0800

    Added some style and scripts

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const rates = document.querySelectorAll('.rate');
+let rate;
 
-rates.forEach((rate) =>
-  rate.addEventListener('click', () => {
-    rate.classList.remove('selected');
-    if (!rate.classList.contains('selected')) {
-      rate.classList.add('selected');
+function changeStyle(num) {
+  for (let i = 0; i < 5; i++) {
+    if (Number(num) == i + 1) {
+      rates[i].style = 'color: white; background: hsl(216, 12%, 54%);';
+      rate = rates[i];
+    } else {
+      rates[i].style = '';
     }
-  })
-);
+  }
+}
+
+function submitHandler() {
+  document.querySelector('#rate-card').classList.add('hide');
+  document.querySelector('#result').classList.add('show');
+  document.querySelector(
+    '.label'
+  ).textContent = `You selected ${rate.textContent} out of 5`;
+}
