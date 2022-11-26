b04d16540663b9d679b599d638479775201e2dfb
CodeWithSubaru <francis.cloydm@gmail.com>
2022-11-26 15:40:42 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import { ref } from 'vue';
+import CardComponent from './components/CardComponent.vue';
+
+export default {
+  components: { CardComponent },
+
+  setup() {
+    const social = ref([
+      { id: 1, number: '80K', desc: 'Followers' },
+      { id: 2, number: '803K', desc: 'Likes' },
+      { id: 3, number: '1.4K', desc: 'Photos' },
+    ]);
+
+    return { social };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <card-component :social="social" />
+  </main>
+</template>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,40 @@
+<script>
+import profilePic from '../assets/images/image-victor.jpg';
+import bgPatternCard from '../assets/images/bg-pattern-card.svg';
+import bgPatternTop from '../assets/images/bg-pattern-top.svg';
+import bgPatternBottom from '../assets/images/bg-pattern-bottom.svg';
+
+export default {
+  props: ['social'],
+  setup() {
+    return { profilePic, bgPatternCard, bgPatternTop, bgPatternBottom };
+  },
+};
+</script>
+
+<template>
+  <img :src="bgPatternTop" alt="background pattern" class="bg-pattern-top" />
+  <img
+    :src="bgPatternBottom"
+    alt="background pattern"
+    class="bg-pattern-bottom"
+  />
+  <section class="profile-card">
+    <img
+      :src="bgPatternCard"
+      alt="profile background"
+      class="profile-card__bg"
+    />
+    <img :src="profilePic" alt="profile picture" class="profile-card__img" />
+    <h2 class="profile-card__name">
+      Victor Crest <span class="profile-card__age"> 26</span>
+    </h2>
+    <p class="profile-card__city">London</p>
+    <footer class="profile-card__footer">
+      <div class="profile-card__social" v-for="info of social" :key="info.id">
+        <h2 class="profile-card__numbers">{{ info.number }}</h2>
+        <small class="profile-card__description"> {{ info.desc }}</small>
+      </div>
+    </footer>
+  </section>
+</template>
